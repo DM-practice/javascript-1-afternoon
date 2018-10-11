fcfa0f25585f8e26439c1ef31386693e46591711
Harlan Heath <harlandheath@gmail.com>
2018-10-11 16:19:47 -0500

    finishing practice

--- practice.js
+++ practice.js
@@ -5,28 +5,32 @@
 //greeting should return the string "Hello, "
 //plus the value of the name parameter.
 
-//Code here
+function greeting(name) {
+  return "Hello, " + name;
+}
 
 //////////////////PROBLEM 2////////////////////
 
 //Rewrite the function greeting as a function expression.
 //Name it newGreeting.
 
-//Code Here
+var newGreeting = function(name) {
+  return "Hello, " + name;
+};
 
 //////////////////PROBLEM 3////////////////////
 
 //Rewrite the function greeting as an arrow function.
 //Name it finalGreeting.
 
-//Code Here
+var finalGreeting = name => "Hello, " + name;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+var groceries = ["apples", "milk", "eggs", "bread"];
 
 //Write a function called doubleCheck that takes in an array
 //as a parameter.
@@ -34,7 +38,14 @@
 //If the array does not contain "chocolate", add "chocolate".
 //doubleCheck should return the array.
 
-//Code Here
+function doubleCheck(arr1) {
+  for (i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== "chocolate") {
+      arr1.push("chocolate");
+    }
+  }
+  return arr1;
+}
 
 //////////////////PROBLEM 5////////////////////
 
@@ -43,21 +54,28 @@
 //name (a string), color: (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+var dog = {
+  name: "dog",
+  color: "red",
+  age: 3,
+  goodBoy: true
+};
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
-//Code Here
+var devMountainClassPet = dog.name;
 
 //Add a method to dog called bark.
 //The value of bark should be a function that returns the string "Woof woof".
 
-//Code Here
+dog.bark = function() {
+  return "woof woof";
+};
 
 //Store the result of invoking the bark method in a variable called ruff.
 
-//Code Here
+var ruff = dog.bark();
 
 //////////////////PROBLEM 6////////////////////
 
@@ -70,7 +88,10 @@
 
 //Return mySum.
 
-//Code Here
+function looper(arr1) {
+  mySum = 0;
+  for (i = 0; i < arr1.length; i++) {}
+}
 
 //////////////////PROBLEM 7////////////////////
 
